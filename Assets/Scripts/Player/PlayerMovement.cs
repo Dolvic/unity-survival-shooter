184ad7fcb262d92382f43cfe039b68fa6b031bc5
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-08-28 20:58:35 -0400

    Setup player character
    The character model was imported from the assets. Animation and controls
    were setup as well.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,58 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+    public void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        Turn();
+        Animate(horizontal, vertical);
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0f, vertical);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void Turn()
+    {
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+        if (!Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            return;
+        }
+
+        var playerToMouse = floorHit.point - transform.position;
+        playerToMouse.y = 0f;
+
+        var newRotation = Quaternion.LookRotation(playerToMouse);
+        playerRigidbody.MoveRotation(newRotation);
+    }
+
+    private void Animate(float horizontal, float vertical)
+    {
+        var walking = horizontal != 0f || vertical != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
 }
